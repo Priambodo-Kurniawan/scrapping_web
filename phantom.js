f07a200a1d48ec207ef26af4f21491822641eb4f
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-08-29 21:36:50 +0700

    scraping based on steps on phantomjs

--- phantom.js
+++ phantom.js
@@ -1,68 +1,65 @@
-var page = require('webpage').create();
-var testindex = 0, loadInProgress = false;
+var page = new WebPage(), testindex = 0, loadInProgress = false;
+
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
 
 page.onLoadStarted = function() {
   loadInProgress = true;
-  console.log('load started');
-}
+  console.log("load started");
+};
 
 page.onLoadFinished = function() {
   loadInProgress = false;
-  console.log(('load finished'));
-}
+  console.log("load finished");
+};
 
 var steps = [
   function() {
-    page.open('https://www.aliexpress.com/');
+    //Load Login Page
+    page.open("https://www.aliexpress.com/");
   },
   function() {
-    page.evaluate(function() {
-      var searchInput = document.getElementById('search-key');
-      console.log('searchInput');
-      searchInput.value='32544859436';
-      return
-    })
-  },
-  function() {
-    page.evaluate(function() {
-      var buttonSearch = document.getElementsByClassName('search-button');
-      for(var i=0; i< buttonSearch.length; i++) {
-        if(buttonSearch[i].getAttribute('type') == 'submit'){
-          buttonSearch[i].submit();
-          return;
-        }
-      }
-    })
-  },
-  function() {
-    page.evaluate(function() {
-      console.log(document.querySelectorAll('html')[0].outerHTML);
+    //Search Product By Id
+    page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function() {
+      page.evaluate(function() {
+        var idProduct = '32544859436'
+        $("#search-key").val(idProduct);
+        $("#form-searchbar > div.searchbar-operate-box > input").click();
+      });
     });
+  },
+  function() {
+    //Load Product Page
+    page.evaluate(function() {
+      console.log('load Product Page Success');
+    });
+  },
+  function() {
+    //Scrap data
+    page.evaluate(function() {
+      var idProduct = '32544859436'
+      var product = {}
+      product.id = idProduct;
+      product.pictures = []
+      var img = document.getElementsByClassName('img-thumb-item')
+      for(var i = 0; i<img.length; i++){
+        product.pictures.push(img[i].baseURI)
+      }
+      console.log(JSON.stringify(product));
+    })
   }
 ];
 
+
 interval = setInterval(function() {
-  if (!loadInProgress && typeof steps[testindex] == 'function') {
-    console.log('step ' + (testindex + 1));
+  if (!loadInProgress && typeof steps[testindex] == "function") {
+    console.log("step " + (testindex + 1));
     steps[testindex]();
     testindex++;
   }
-  if (typeof steps[testindex] !== 'function') {
-    console.log('test complete!');
+  if (typeof steps[testindex] != "function") {
+    console.log("test complete!");
     phantom.exit();
   }
-}, 50)
-// console.log('The default user agent is ' + page.settings.userAgent);
-// page.settings.userAgent = 'SpecialAgent';
-
-// page.open('https://www.google.co.id/', function(status) {
-//   if (status !== 'success') {
-//     console.log('Unable to access network');
-//   } else {
-//     var ua = page.evaluate(function() {
-//       return document.getElementById('_eEe').textContent;
-//     });
-//     console.log(ua);
-//   }
-//   phantom.exit();
-// });
+}, 50);
